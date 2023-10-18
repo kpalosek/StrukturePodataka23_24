9ce69e2fe124c99242fb8beeea08e74054c3d812
kpalosek <kpalos00@fesb.hr>
2023-10-18 17:18:25 +0200

    vjezba 1 pocetak

--- vjezba1/vjezba1/vjezba1.c
+++ vjezba1/vjezba1/vjezba1.c
@@ -1,8 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX_SIZE (50)
+#define MAX_LINE (1024)
+
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
 
 int main()
 {
-	printf("Hello world!\n");
+	int noRows = 0;
+	
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+
+
+	filePointer = fopen("Student.txt", "r");
+	if (!filePointer) { //isto kao ==NULL
+		printf("File isn't opened! \n");
+		return -1;
+	}
+	while (!feof(filePointer))
+	{
+		fgets(buffer, MAX_LINE, filePointer);
+		noRows++;
+	}
+	printf("%d", noRows);
+	fclose(filePointer);
 
 	return 0;
 }
